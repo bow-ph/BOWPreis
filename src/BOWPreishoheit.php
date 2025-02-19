99884afaf28413c2e41659dc682ed301f298d25c
Janis Mednis <jm@bow-agentur.de>
2025-02-19 17:31:47 +0100

    monolog.yaml added

--- src/BOWPreishoheit.php
+++ src/BOWPreishoheit.php
@@ -5,9 +5,24 @@ namespace BOW\Preishoheit;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+// ADD THESE IMPORTS
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class BOWPreishoheit extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        // Adjust the path if your monolog.yaml is in a different location.
+        // For example: __DIR__ . '/Resources/config/packages' must point 
+        // to the exact folder containing monolog.yaml
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config/packages'));
+        $loader->load('monolog.yaml');
+    }
+
     public function install(InstallContext $installContext): void
     {
         parent::install($installContext);
@@ -16,11 +31,11 @@ class BOWPreishoheit extends Plugin
     public function uninstall(UninstallContext $uninstallContext): void
     {
         parent::uninstall($uninstallContext);
-        
+
         if ($uninstallContext->keepUserData()) {
             return;
         }
-        
-        // Clean up code will be added here
+
+        // Clean up code can go here
     }
 }
