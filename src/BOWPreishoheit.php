4eda1caa64057df0950c5d4701b85522e1d733a1
bow-ph <131660069+bow-ph@users.noreply.github.com>
2025-02-27 14:14:51 +0100

    Fix problem with the container builder
    fix container builder
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/bow-ph/BOWPreishoheit?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/BOWPreishoheit.php
+++ src/BOWPreishoheit.php
@@ -6,12 +6,14 @@ use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use BOW\Preishoheit\DependencyInjection\BOWPreishoheitExtension;
 
 class BOWPreishoheit extends Plugin
 {
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+        $container->registerExtension(new BOWPreishoheitExtension());
     }
 
     public function install(InstallContext $installContext): void
