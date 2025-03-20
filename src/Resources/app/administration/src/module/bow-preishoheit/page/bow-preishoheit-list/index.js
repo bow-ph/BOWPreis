98f3182e313c6fca3183073b770edeab2cc83926
ph <ph@bow-agentur.de>
2025-03-20 10:29:36 +0100

    fix:

--- src/Resources/app/administration/src/module/bow-preishoheit/page/bow-preishoheit-list/index.js
+++ src/Resources/app/administration/src/module/bow-preishoheit/page/bow-preishoheit-list/index.js
@@ -1,20 +1,17 @@
 import template from './bow-preishoheit-list.html.twig';
-
-const { Component, Mixin } = Shopware;
+const { Component } = Shopware;
 const { Criteria } = Shopware.Data;
 
 Component.register('bow-preishoheit-list', {
     template,
 
-    inject: [
-        'repositoryFactory'
-    ],
+    inject: ['repositoryFactory'],
 
     data() {
         return {
             isLoading: false,
-            products: null,
-            criteria: new Criteria(),
+            products: [],
+            criteria: new Criteria(1, 25),
             total: 0
         };
     },
@@ -48,15 +45,6 @@ Component.register('bow-preishoheit-list', {
         }
     },
 
-    data() {
-        return {
-            isLoading: false,
-            products: [],
-            criteria: new Criteria(1, 25),
-            total: 0
-        };
-    },
-
     created() {
         this.loadProducts();
     },
@@ -64,21 +52,40 @@ Component.register('bow-preishoheit-list', {
     methods: {
         loadProducts() {
             this.isLoading = true;
-
             this.criteria.addAssociation('product');
 
-            this.repositoryFactory.create('bow_preishoheit_product')
+            this.productRepository
                 .search(this.criteria, Shopware.Context.api)
                 .then(({ items, total }) => {
                     this.products = items;
                     this.total = total;
                 })
+                .catch((error) => {
+                    this.createNotificationError({
+                        title: 'Fehler',
+                        message: error.message
+                    });
+                })
                 .finally(() => {
                     this.isLoading = false;
                 });
-    },
+        },
 
         refreshList() {
+            this.loadProducts();
+        },
+
+        onPageChange({ page = 1, limit = 25 }) {
+            this.criteria.page = page;
+            this.criteria.limit = limit;
+            this.loadProducts();
+        },
+
+        onSortColumn({ sortBy, sortDirection }) {
+            this.criteria.resetSorting();
+            if (sortBy) {
+                this.criteria.addSorting(Criteria.sort(sortBy, sortDirection));
+            }
             this.loadProducts();
         }
     }
